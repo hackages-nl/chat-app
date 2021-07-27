e9fa30e058fb5c38107c6b1daeb24a25e0adde4e
Davy Engone <davy@hackages.io>
2021-07-27 16:38:05 +0200

    added some mock data

---
+++ data.js
@@ -0,0 +1,141 @@
+export const user = {
+  userId: 000,
+  profilePicture: "assets/img/profile01.png",
+  username: "Jawad Saouti",
+  lastSeenTime: "3:35 PM",
+};
+
+export const chatList = [
+  {
+    userId: 001,
+    profilePicture: "assets/img/profile03.png",
+    username: "Boris Doe",
+    lastSeenTime: "4:32 PM",
+    lastMessage: "Let’s meet on 3pm today",
+    conversation: [
+      {
+        sender: 001,
+        message:
+          "I’m still eat in angkringan. Maybe 15 minutes later I will send the files to you bro.",
+        time: "12:21 PM",
+      },
+      {
+        sender: 000,
+        message:
+          "I’m still eat in angkringan. Maybe 15 minutes later I will send the files to you bro.",
+        time: "12:21 PM",
+      },
+      {
+        sender: 001,
+        message:
+          "I’m still eat in angkringan. Maybe 15 minutes later I will send the files to you bro.",
+        time: "12:21 PM",
+      },
+      {
+        sender: 001,
+        message:
+          "I’m still eat in angkringan. Maybe 15 minutes later I will send the files to you bro.",
+        time: "12:21 PM",
+      },
+    ],
+  },
+  {
+    userId: 002,
+    profilePicture: "assets/img/profile01.png",
+    username: "David Doe",
+    lastSeenTime: "3:32 PM",
+    lastMessage: "Let’s meet on 3pm today",
+    conversation: [
+      {
+        sender: 002,
+        message:
+          "I’m still eat in angkringan. Maybe 15 minutes later I will send the files to you bro.",
+        time: "12:21 PM",
+      },
+      {
+        sender: 000,
+        message:
+          "I’m still eat in angkringan. Maybe 15 minutes later I will send the files to you bro.",
+        time: "12:21 PM",
+      },
+      {
+        sender: 002,
+        message:
+          "I’m still eat in angkringan. Maybe 15 minutes later I will send the files to you bro.",
+        time: "12:21 PM",
+      },
+      {
+        sender: 002,
+        message:
+          "I’m still eat in angkringan. Maybe 15 minutes later I will send the files to you bro.",
+        time: "12:21 PM",
+      },
+    ],
+  },
+  {
+    userId: 003,
+    profilePicture: "assets/img/profile02.png",
+    username: "Xavier Martine",
+    lastSeenTime: "4:30 PM",
+    lastMessage: "Let’s meet on 3pm today",
+    conversation: [
+      {
+        sender: 003,
+        message:
+          "I’m still eat in angkringan. Maybe 15 minutes later I will send the files to you bro.",
+        time: "12:21 PM",
+      },
+      {
+        sender: 000,
+        message:
+          "I’m still eat in angkringan. Maybe 15 minutes later I will send the files to you bro.",
+        time: "12:21 PM",
+      },
+      {
+        sender: 003,
+        message:
+          "I’m still eat in angkringan. Maybe 15 minutes later I will send the files to you bro.",
+        time: "12:21 PM",
+      },
+      {
+        sender: 003,
+        message:
+          "I’m still eat in angkringan. Maybe 15 minutes later I will send the files to you bro.",
+        time: "12:21 PM",
+      },
+    ],
+  },
+  {
+    userId: 004,
+    profilePicture: "assets/img/profile03.png",
+    username: "Henri Ford",
+    lastSeenTime: "2:00 PM",
+    lastMessage: "The web app was awesome!",
+    conversation: [
+      {
+        sender: 004,
+        message:
+          "I’m still eat in angkringan. Maybe 15 minutes later I will send the files to you bro.",
+        time: "12:21 PM",
+      },
+      {
+        sender: 000,
+        message:
+          "I’m still eat in angkringan. Maybe 15 minutes later I will send the files to you bro.",
+        time: "12:21 PM",
+      },
+      {
+        sender: 004,
+        message:
+          "I’m still eat in angkringan. Maybe 15 minutes later I will send the files to you bro.",
+        time: "12:21 PM",
+      },
+      {
+        sender: 004,
+        message:
+          "I’m still eat in angkringan. Maybe 15 minutes later I will send the files to you bro.",
+        time: "12:21 PM",
+      },
+    ],
+  },
+];
